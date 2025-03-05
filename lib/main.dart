b79d3ab6f2120a8f046bdb6c184890c1825b2a0c
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2025-03-05 19:40:38 +0500

    Add EmberPlayer and EmberQuestGame classes with initial game setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:ember_quest/ember_quest_game.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+  runApp(GameWidget<EmberQuestGame>.controlled(gameFactory: EmberQuestGame.new));
 }
