0de16b2fc01cb203b4c9ab9219384867bbfa22f3
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2025-03-05 19:59:10 +0500

    Add game segment management and water enemy class

--- lib/ember_quest_game.dart
+++ lib/ember_quest_game.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
 
 import 'package:ember_quest/actors/emper_player.dart';
+import 'package:ember_quest/actors/water_enemy.dart';
+import 'package:ember_quest/managers/segment_manager.dart';
+import 'package:ember_quest/objects/ground_block.dart';
+import 'package:ember_quest/objects/platform_block.dart';
+import 'package:ember_quest/objects/star.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
@@ -21,9 +26,31 @@ class EmberQuestGame extends FlameGame {
 
     camera.viewfinder.anchor = Anchor.topLeft;
 
-    _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
-    world.add(_ember);
+    initializeGame();
 
     return super.onLoad();
+  }
+
+  void _loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+        case PlatformBlock:
+        case Star:
+        case WaterEnemy:
+      }
+    }
+  }
+
+  void initializeGame() {
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      _loadGameSegments(i, (i * 640).toDouble());
+    }
+
+    _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
+    world.add(_ember);
   }
 }
